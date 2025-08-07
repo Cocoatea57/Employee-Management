b1aa3ce8e25d72e3e67d66877049147010758ae4
cocoatea57 <frankowusuansah57@gmail.com>
2025-08-07 00:11:45 +0000

    navbar semi-done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-        <nav className='flex justify-around '>
+        <nav className='flex justify-around bg-blue-400 shadow-xl p-2 mx-2 '>
             <div>
-                <img src={kenny} alt="division" className='h-[20px]'/>
+                <p>FraNel company LTD</p>
             </div>
-            <div>
+            <div className='flex gap-4'>
                 <Link to='/Employee' >Employee</Link>
                 <Link to='/LeavePage' >LeavePage</Link>
+                <img src={kenny} alt="division" className='shadow-white shadow-2xl text-white w-[6px] h-[20px]'/>
                 <Link to='/Register'>Register</Link>
                 <Link to='/Manager'>Manager</Link>
             </div>
