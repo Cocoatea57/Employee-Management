00a9464495b3d3f56948b18e4ddacde497abc081
cocoatea57 <frankowusuansah57@gmail.com>
2025-08-07 22:12:56 +0000

    started registration for

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div>
-        <nav className='flex justify-around bg-blue-400 shadow-xl  p-2 '>
+        <nav className='flex justify-around bg-blue-400 shadow-xl p-2 '>
             <div>
                 <marquee className='shadow-white shadow-xl border-t-1 border-white px-2'><strong>FraNel company LTD </strong></marquee>
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,9 @@ function Register() {
 
     
   return (
-    <div>Register</div>
+    <div>
+      
+    </div>
   )
 }
 
