74955608bbcaef8d3871c50eb81f7d7b08f5975a
cocoatea57 <frankowusuansah57@gmail.com>
2025-08-07 21:12:14 +0000

    improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ function Navbar() {
     <div>
         <nav className='flex justify-around bg-blue-400 shadow-xl p-2 mx-2 '>
             <div>
-                <p className='shadow-white shadow-xl border-t-1 border-white'><strong>FraNel company LTD </strong></p>
+                <marquee className='shadow-white shadow-xl border-t-1 border-white px-2'><strong>FraNel company LTD </strong></marquee>
             </div>
             <div className='flex gap-4 shadow-lg'>
-                <Link to='/Employee' className='font-semibold shadow-white shadow-xl border px-2 border-white'>Employees</Link>
+                <Link to='/Employee' className='font-semibold shadow-white shadow-xl border px-2 border-white hover:bg-green-500'>Employees</Link>
                 <img src={kenny} alt="division" className='shadow-white shadow-2xl text-white w-[5px] h-[30px]'/>
-                <Link to='/LeavePage' className='font-semibold shadow-white shadow-xl border px-2 border-white'>Request Leave</Link>
+                <Link to='/LeavePage' className='font-semibold shadow-white shadow-xl border px-2 border-white hover:bg-green-500'>Request Leave</Link>
             </div>
         </nav>
     </div>
