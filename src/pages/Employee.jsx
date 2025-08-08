69a4fea1a0af7197be3d0ff25fa0e544e971ee1e
cocoatea57 <frankowusuansah57@gmail.com>
2025-08-08 21:15:27 +0000

    registration form

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useState} from "react";
 import { v4 as uuid } from "uuid";
 import EmployeeDetails from "../components/EmployeeDetails";
 import EmployeeList from "../components/EmployeeList";
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,7 +5,37 @@ function Register() {
     
   return (
     <div>
-      
+      <h1>Add Employee</h1>
+      <div>
+        <h6>Fill details</h6>
+        <form action="">
+          <div>
+            <label htmlFor="">First Name</label>
+            <input type="text" placeholder='Firstname' />
+          </div>
+          <div>
+            <label htmlFor="">Last Name</label>
+            <input type="text" placeholder='Lastname' />
+          </div>
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder='example@email.com' />
+          </div>
+          <div>
+            <label htmlFor="">Role</label>
+            <input list='roles' placeholder='Select role' />
+            <datalist id='roles' >
+                  <option value="Snr Dev">Senior Developer</option>
+                  <option value="mid dev">Intermediate Dev</option>
+                  <option value="jnr dev">Junior Dev</option>
+            </datalist>
+
+          </div>
+          <div>
+            n
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
