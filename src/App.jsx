0c5f627850b0622c6f506eeb04316ccd8479b35e
neltarget <mart.phil23@gmail.com>
2025-08-09 07:23:06 +0000

    updated App.jsx and Employee.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,69 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes , Route } from 'react-router-dom'
-import Manager from './pages/Manager'
-import LeavePage from './pages/LeavePage'
-import Register from './pages/Register'
-import Navbar from './components/Navbar'
-import Employee from './pages/Employee'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import Manager from "./pages/Manager";
+import LeavePage from "./pages/LeavePage";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Employee from "./pages/Employee";
 
 function App() {
+  const [employees, setEmployees] = useState([
+    {
+      firstName: "Frank",
+      lastName: "Owusu",
+      email: "frankowusu@email.com",
+      age: 25,
+      address: "St Palm Avenue",
+      department: "Software",
+      role: "Senior Dev",
+      salary: 15000,
+      hireDate: new Date().toLocaleDateString(),
+      id: uuid(),
+    },
+  ]);
+
+  //  Register Employee
+  const addNewEmployee = (newEmployee) => {
+    setEmployees([...employees, { ...newEmployee, id: uuid() }]);
+  };
+
+  // Edit Employee
+  const editEmployee = (employeeId, newDetails) => {
+    const updated = employees.map((employee) =>
+      employee.id === employeeId ? { ...employee, ...newDetails } : employee
+    );
+    setEmployees(updated);
+  };
+
+  //  Delete Employee
+  const deleteEmployee = (employeeId) => {
+    const updated = employees.filter((employee) => employee.id !== employeeId);
+    setEmployees(updated);
+  };
+
   return (
-   <Router>
-    <Navbar/>
-    <Routes>
-      <Route path='/Employee' element={<Employee/>}/>
-      <Route path='/Manager' element={<Manager/>}/>
-      <Route path='/LeavePage' element={<LeavePage/>}/>
-      <Route path='/Register' element={<Register/>}/>
-    </Routes>
-   </Router>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/Employee"
+          element={
+            <Employee
+              employees={employees}
+              addNewEmployee={addNewEmployee}
+              editEmployee={editEmployee}
+              deleteEmployee={deleteEmployee}
+            />
+          }
+        />
+        <Route path="/Manager" element={<Manager />} />
+        <Route path="/LeavePage" element={<LeavePage />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,48 +1,17 @@
-import {React,useState} from "react";
-import { v4 as uuid } from "uuid";
+import React from "react";
+import { useState } from "react";
 import EmployeeDetails from "../components/EmployeeDetails";
 import EmployeeList from "../components/EmployeeList";
-function Employee() {
-  const [employees, setEmployees] = useState([
-    {
-      firstName: "Frank",
-      lastName: "Owusu",
-      email: "frankowusu@email.com",
-      age: 25,
-      address: "St Palm Avenue",
-      department: "Software",
-      role: "Senior Dev",
-      salary: 15000,
-      hireDate: new Date().toLocaleDateString(),
-      id: uuid(),
-    },
-  ]);
-
-  //  Register Employee
-  const addNewEmployee = (newEmployee) => {
-    setUsers([...employees, { ...newEmployee, id: uuid() }]);
-  };
-
-  // Edit Employee
-  const editEmployee = (employeeId, newDetails) => {
-    const updated = employees.map((employee) =>
-      employee.id === employeeId ? { ...employee, ...newDetails } : employee
-    );
-    setEmployees(updated);
-  };
+function Employee({ employees, addNewEmployee, editEmployee, deleteEmployee }) {
   return (
-    <>
-      {/* left pane - register employee + employee list */}
-      <div>
-        {/* search and sort functionality */}
-
-        {/* employee list */}
-        <EmployeeList/>
-      </div>
-
-      {/* right pane - employee detail */}
-      <EmployeeDetails/>
-    </>
+    <div className="flex gap-2">
+      <EmployeeList employees={employees} addNewEmployee={addNewEmployee} />
+      <EmployeeDetails
+        employees={employees}
+        editEmployee={editEmployee}
+        deleteEmployee={deleteEmployee}
+      />
+    </div>
   );
 }
 
