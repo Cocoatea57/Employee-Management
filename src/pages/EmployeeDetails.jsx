1e27e180d9b0b951ee351aed55b11b1d905ef865
neltarget <mart.phil23@gmail.com>
2025-08-12 04:53:46 +0000

    saved changes before pulling employee details styling

--- src/pages/EmployeeDetails.jsx
+++ src/pages/EmployeeDetails.jsx
@@ -21,24 +21,42 @@ function EmployeeDetails() {
   return (
     <div>
       <h2>Employee Details</h2>
+      {/* profile image  */}
+      <div>{employee.preview}</div>
       <p>
         <strong>Full Name:</strong> {employee.firstName} {employee.lastName}
       </p>
-      <p>
-        <strong>Role:</strong> {employee.role}
-      </p>
-      <p>
-        <strong>Email:</strong> {employee.email}
-      </p>
-      <p>
-        <strong>Department:</strong> {employee.department}
-      </p>
-      <p>
-        <strong>Salary:</strong> ${employee.salary}
-      </p>
-      <p>
-        <strong>Hire Date:</strong> {employee.hireDate}
-      </p>
+      <div className="flex">
+        {/* Contact Details info */}
+        <div>
+          <p>
+            <strong>Email:</strong> {employee.email}
+          </p>
+          <p>
+            <strong>Age:</strong> {age.email}
+          </p>
+          <p>
+            <strong>Address:</strong> {address.email}
+          </p>
+        </div>
+
+        {/* Job info */}
+        <div>
+          <p>
+            <strong>Role:</strong> {employee.role}
+          </p>
+
+          <p>
+            <strong>Department:</strong> {employee.department}
+          </p>
+          <p>
+            <strong>Salary:</strong> ${employee.salary}
+          </p>
+          <p>
+            <strong>Hire Date:</strong> {employee.hireDate}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
